8c4893ed2a75cad68ee31c945237c5f623fd4ef2
VaheMeekz <vahe.mkrtchyan.a3@gmail.com>
2022-06-20 15:17:29 +0400

    config

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
-// export const baseUrl = "https://volunteer002.herokuapp.com/api/v1";
-export const baseUrl = "http://localhost:5000/api/v1";
-export const staticUrl = "http://localhost:5000"
+export const baseUrl = "https://volunteer002.herokuapp.com/api/v1";
+export const staticUrl = "https://volunteer002.herokuapp.com"
+// export const baseUrl = "http://localhost:5000/api/v1";
+// export const staticUrl = "http://localhost:5000"
 export const token = JSON.parse(localStorage.getItem("myToken"));
 export const email = JSON.parse(localStorage.getItem("email"));
 export const CLOUDINARY_PRESENT = "qtfygrwo";
